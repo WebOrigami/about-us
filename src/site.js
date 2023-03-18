7ef1693fdb25263df199ecb0024bb76b92703338
Jan Miksovsky <jan@miksovsky.com>
2023-03-18 11:50:19 -0700

    Generate HTML more directly, without relying on scope

--- src/site.js
+++ src/site.js
@@ -1,43 +1,43 @@
 import {
+  FileLoadersTransform,
   FilesGraph,
-  FileTreeTransform,
-  InvokeFunctionsTransform,
   MapExtensionsGraph,
   MapInnerKeysGraph,
   MapValuesGraph,
-  ObjectGraph,
 } from "@graphorigami/origami";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import thumbnail from "./thumbnail.js";
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
-const files = new (FileTreeTransform(FilesGraph))(dirname);
+const files = new (FileLoadersTransform(FilesGraph))(dirname);
 
 const assets = await files.get("assets");
 const images = await files.get("images");
 const indexTemplate = await files.get("index.ori");
-const indexTemplateFn = indexTemplate.toFunction();
+const indexHtmlFn = indexTemplate.toFunction();
 const personTemplate = await files.get("person.ori");
+const personHtmlFn = personTemplate.toFunction();
 const teamData = await files.get("teamData.yaml");
 
+const title = "Our Amazing Team";
+const indexHtml = indexHtmlFn({ teamData, title });
 const thumbnails = new MapValuesGraph(images, thumbnail);
 const teamByName = new MapInnerKeysGraph(teamData, (value) =>
   value.get("name")
 );
-const team = new MapExtensionsGraph(teamByName, personTemplate, {
-  extension: "->html",
-});
+const team = new MapExtensionsGraph(
+  teamByName,
+  (person) => personHtmlFn({ person, title }),
+  {
+    extension: "->html",
+  }
+);
 
-const graph = new (FileTreeTransform(InvokeFunctionsTransform(ObjectGraph)))({
-  public: {
-    assets,
-    images,
-    "index.html": indexTemplateFn,
-    team,
-    thumbnails,
-  },
-  title: "Our Amazing Team",
-});
-
-export default graph;
+export default {
+  assets,
+  images,
+  "index.html": indexHtml,
+  team,
+  thumbnails,
+};
