f22ab1183917b3a9bff8632fdc2a17316003b265
Jan Miksovsky <jan@miksovsky.com>
2023-03-18 12:37:20 -0700

    Use FilesGraph directly; load templates explicitly

--- src/site.js
+++ src/site.js
@@ -1,34 +1,33 @@
 import {
-  FileLoadersTransform,
+  ExplorableGraph,
   FilesGraph,
   MapExtensionsGraph,
   MapInnerKeysGraph,
   MapValuesGraph,
+  OrigamiTemplate,
 } from "@graphorigami/origami";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import thumbnail from "./thumbnail.js";
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
-const files = new (FileLoadersTransform(FilesGraph))(dirname);
+const files = new FilesGraph(dirname);
 
 const assets = await files.get("assets");
 const images = await files.get("images");
-const indexTemplate = await files.get("index.ori");
-const indexHtmlFn = indexTemplate.toFunction();
-const personTemplate = await files.get("person.ori");
-const personHtmlFn = personTemplate.toFunction();
-const teamData = await files.get("teamData.yaml");
+const indexTemplate = new OrigamiTemplate(await files.get("index.ori"));
+const personTemplate = new OrigamiTemplate(await files.get("person.ori"));
+const teamData = ExplorableGraph.fromYaml(await files.get("teamData.yaml"));
 
 const title = "Our Amazing Team";
-const indexHtml = indexHtmlFn({ teamData, title });
+const indexHtml = indexTemplate.apply({ teamData, title });
 const thumbnails = new MapValuesGraph(images, thumbnail);
 const teamByName = new MapInnerKeysGraph(teamData, (value) =>
   value.get("name")
 );
 const team = new MapExtensionsGraph(
   teamByName,
-  (person) => personHtmlFn({ person, title }),
+  (person) => personTemplate.apply({ person, title }),
   {
     extension: "->html",
   }
